dd9508fae3bbbbc106d04c87f3c0e29b066f0e8f
Bill Beers <nothingatall544@gmail.com>
2013-02-18 00:44:14 -0500

    re added

---
+++ CyclometerReceiver.cpp
@@ -0,0 +1,16 @@
+/*
+ * CyclometerReceiver.cpp
+ *
+ *  Created on: Feb 13, 2013
+ *      Author: wkb6223
+ */
+
+#include "CyclometerReceiver.h"
+
+CyclometerReceiver::CyclometerReceiver(){
+
+}
+
+void CyclometerReceiver::handleEvent ( Event event ){
+
+}
